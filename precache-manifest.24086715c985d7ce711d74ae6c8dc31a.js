71f017b975e7b0d7a28f418e16fb76ec8c3e4247
Karina Gaulin <karina@domandtom.com>
2019-03-13 10:28:08 -0400

    Updates

---
+++ precache-manifest.24086715c985d7ce711d74ae6c8dc31a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9695152e3117de44e600",
+    "url": "/video/static/js/runtime~main.e0a31693.js"
+  },
+  {
+    "revision": "b8eed229a2a0545debfd",
+    "url": "/video/static/js/main.49309e3a.chunk.js"
+  },
+  {
+    "revision": "781cb53bf22462b2f508",
+    "url": "/video/static/js/2.11abe5f7.chunk.js"
+  },
+  {
+    "revision": "8588eb442241e65d2fbe822c73c32bce",
+    "url": "/video/index.html"
+  }
+];
