9f86c5d1f66da94e8ed1542107f3433d6698d3f8
Karina Gaulin <karina@domandtom.com>
2019-03-13 15:44:49 -0400

    Finish main parts

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -15,17 +16,26 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log('From the App', video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
        <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} /> 
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos} 
-          />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos} 
+                />
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
